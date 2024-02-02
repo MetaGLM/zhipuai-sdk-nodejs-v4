0291a6efc6ef1f03f7016074d75884a64093bdd3
winily <18934086807@163.com>
2024-02-02 18:25:59 +0800

    feat: readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# 智谱大模型开放接口SDK
+智谱开放平台大模型接口 Nodejs SDK (Big Model API SDK in Nodejs)，让开发者更便捷的调用智谱开放API
--- test/test.js
+++ test/test.js
@@ -8,9 +8,11 @@ const { ZhipuAI } = require('../dist/cjs/index.js');
             { "role": "system", "content": "你是一个乐于解答各种问题的助手，你的任务是为用户提供专业、准确、有见地的建议。" },
             { "role": "user", "content": "我对太阳系的行星非常感兴趣，特别是土星。请提供关于土星的基本信息，包括其大小、组成、环系统和任何独特的天文现象。" },
         ],
-        // stream: true,
+        stream: true,
     })
     console.log(result.data, "result.data")
-    console.log(result.data.choices, "result.data")
-    console.log(result.data.choices[0].message, "result.data")
+
+    result.data.on('data', (data) => {
+        console.log(data.toString(), "result.data")
+    })
 })()
